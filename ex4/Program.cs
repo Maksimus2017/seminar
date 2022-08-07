fb0769b9e1d50819050b412a8b1f690757802ff4
Maksim <rusant.k2017@yandex.ru>
2022-08-07 18:19:14 +0300

    Четное число

---
+++ ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("введите число ");
+int number = int.Parse(Console.ReadLine());
+int result = number%2;
+if (result == 0)
+{
+Console.WriteLine("Четное число");
+}
+else 
+{
+    Console.WriteLine("Нечетное число");
+}
+ 
