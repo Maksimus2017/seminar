23c43abfe6bae3df4eae0e19aa3c688082c379b8
Maksim <rusant.k2017@yandex.ru>
2022-08-07 13:53:47 +0300

    Маша

---
+++ ex2 if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, Саша");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
