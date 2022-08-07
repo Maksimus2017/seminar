c8f8460111d86b0bfcfde2d7444bc6578ead4bd4
Maksim <rusant.k2017@yandex.ru>
2022-08-07 11:41:55 +0300

    Cлучай

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,3 +1,6 @@
 ﻿double number1 = new Random().Next(1, 10); //1 2 3 4 ...9
+Console.WriteLine(number1);
 double number2 = new Random().Next(1, 10);
-Console.WriteLine(number1/number2);
+Console.WriteLine(number2);
+double result = number1+number2;
+Console.WriteLine(result);
