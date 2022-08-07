bf0e4c6d8a871270acdbe8784eb3d05cbfa4a8f5
Maksim <rusant.k2017@yandex.ru>
2022-08-07 11:32:12 +0300

    Случайные числа

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,4 +1,3 @@
-﻿double number1 = 1;
-double number2 = 2;
+﻿double number1 = new Random().Next(1, 10); //1 2 3 4 ...9
+double number2 = new Random().Next(1, 10);
 Console.WriteLine(number1/number2);
- 
