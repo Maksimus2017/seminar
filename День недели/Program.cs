3872b1244dd78ca33c50835cf12fc49a9905b996
Maksim <rusant.k2017@yandex.ru>
2022-08-11 18:36:09 +0300

    Выходной

---
+++ День недели/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число, обозначающее день недели ");
+int num = int.Parse(Console.ReadLine());
+if (num==6 || num==7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+ else if (num <1 || num>7)
+  {
+    Console.WriteLine("Это не день недели");
+      }
+ 
+ else
+ 
+{
+ Console.WriteLine("Этот день не выходной");
+}
+ 
