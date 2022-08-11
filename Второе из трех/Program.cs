030ff75eaea5ab06676974465a978a9adfc26ad8
Maksim <rusant.k2017@yandex.ru>
2022-08-11 18:41:10 +0300

    2 из трехзначного

---
+++ Второе из трех/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int result=((num%100)/10);
+Console.WriteLine($"Второе число {result} трехзначного числа {num}");
