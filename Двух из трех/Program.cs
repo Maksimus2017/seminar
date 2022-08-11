8add2300a1997eea695837894231ad4b29c9b985
Maksim <rusant.k2017@yandex.ru>
2022-08-11 18:39:05 +0300

    2 из трехзначного

--- Двух из трех/Program.cs
+++ Двух из трех/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Дано трехзначное число убрать второе число в нем и получить двухзначное число.
 
-int num = newRandom().Next(100,1000);
+int num = new Random().Next(100,1000);
 int result =(num/100)*10+num%10;
 Console.WriteLine($" {num}  {result}");
